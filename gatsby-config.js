24f48b530a201b943a5ccdad507d73856b0da02f
Rama Dimasatria <ramadimasatria@gmail.com>
2017-12-21 23:48:34 +0700

    Add wordpress as data source

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Default Starter`
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        baseUrl: 'ssmgatsby.wpengine.com'
+      }
+    }
+  ]
 }
